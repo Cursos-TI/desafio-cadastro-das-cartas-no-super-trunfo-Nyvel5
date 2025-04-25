f39adca6d8f1afc4d4f15c502e9f31fb4c820e4c
Nyvel5 <tassoalbuquerquem@gmail.com>
2025-04-25 01:17:01 +0000

    Atividade Cartas Super Trunfo resolvida

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,16 +7,75 @@
 //Teste larissa
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+        char estado1, estado2, nome1[20], nome2[20];
+        char code1[4], code2[4];
+        int population1, turismo1, population2, turismo2;
+        float area1, pib1, area2, pib2;
+        
+        //estado
+        printf ("INSIRA O ESTADO(DE a A h) DA CIDADE 1:\n");
+          scanf (" %c", &estado1);
+        printf ("INSIRA O ESTADO(DE a A h) DA CIDADE 2:\n");
+          scanf (" %c", &estado2);
+        
+        //codigo
+        printf ("INSIRA O CÓDIGO DA CIDADE 1: \n");
+          scanf (" %s", &code1);
+        printf ("INSIRA O CÓDIGO DA CIDADE 2:\n");
+          scanf (" %s", &code2);
     
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+        //nome
+        printf ("INSIRA O NOME DA CIDADE 1:\n");
+          scanf (" %s", &nome1);
+        printf ("INSIRA O NOME DA CIDADE 2:\n");
+          scanf (" %s", &nome2);
+    
+        //população
+        printf ("INSIRA A POPULAÇÃO DA CIDADE 1:\n");
+          scanf (" %d", &population1);
+        printf ("INSIRA A POPULUÇÃOE DA CIDADE 2:\n");
+          scanf (" %d", &population2);
+    
+        //area
+        printf ("INSIRA A ÁREA DA CIDADE 1:\n");
+          scanf (" %f", &area1);
+        printf ("INSIRA A ÁREA DA CIDADE 2:\n");
+          scanf (" %f", &area2);
+    
+        //PIB
+        printf ("INSIRA O PIB DA CIDADE 1:\n");
+          scanf ("%f", &pib1);
+        printf ("INSIRA O PIB DA CIDADE 2:\n");
+          scanf ("%f", &pib2);
+    
+        //turismo
+        printf ("INSIRA QUANTIDADE DE PONTOS TURÍSTICOS DA CIDADE 1:\n");
+          scanf (" %d", &turismo1);
+        printf ("INSIRA QUANTIDADE DE PONTOS TURÍSTICOS DA CIDADE 2:\n");
+          scanf (" %d", &turismo2);
+    
+    //dados
+        printf ("CIDADE 1: \n");
+        printf ("ESTADO: %c \n", estado1);
+        printf ("CÓDIGO CARTA: %s \n", code1);
+        printf ("NOME: %s \n", nome1);
+        printf ("POPULAÇÂO: %d \n", population1);
+        printf ("AREA: %f km quadrados \n", area1);
+        printf ("PIB: %f bilhões de reais \n", pib1);
+        printf ("PONTOS TURÌSTICOS: %d \n", turismo1);
+        
+        printf ("==============================\n");
+        
+        printf ("CIDADE 2: \n");
+        printf ("ESTADO: %c \n", estado2);
+        printf ("CÓDIGO CARTA: %s \n", code2);
+        printf ("NOME: %s \n", nome2);
+        printf ("POPULAÇÂO: %d \n", population2);
+        printf ("AREA: %f km quadrados \n", area2);
+        printf ("PIB: %f bilhões de reais \n", pib2);
+        printf ("PONTOS TURÌSTICOS: %d \n", turismo2);
+
 
     return 0;
 }
