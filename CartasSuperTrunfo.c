cbc228d7a7c907d9ba97aa1c6746bed07cf32086
Nyvel5 <tassoalbuquerquem@gmail.com>
2025-04-26 02:39:32 +0000

    NIVEL AVANÇADO REALIZADO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,11 +6,14 @@
 // Siga os comentários para implementar cada parte do desafio.
 //Teste larissa
 
-int main() {
-    
-  char  estado1, estado2, nome1[20], nome2[20];
+#include <stdio.h>
+
+int main()
+{
+	char  estado1, estado2, nome1[20], nome2[20];
 	char  code1[4], code2[4];
-	int   population1, turismo1, population2, turismo2;
+	unsigned long int   population1, population2;
+	int   turismo1, turismo2;
 	float area1, pib1, area2, pib2, pibcapita1, pibcapita2, densidade1, densidade2;
 
 //estado
@@ -34,7 +37,7 @@ int main() {
 //populaC'C#o
 	printf ("INSIRA A POPULAÇÃO DA CIDADE 1:\n");
 	scanf (" %d", &population1);
-	printf ("INSIRA A POPULUÇÃO DA CIDADE 2:\n");
+	printf ("INSIRA A POPULAÇÃO DA CIDADE 2:\n");
 	scanf (" %d", &population2);
 
 //area
@@ -60,31 +63,90 @@ int main() {
 
 	densidade1 = population1 / area1;
 	densidade2 = population2 / area2;
+	
+	float superpower1 = population1 + area1 + pib1 + turismo1 + pibcapita1 - densidade1;
+    float superpower2 = population2 + area2 + pib2 + turismo2 + pibcapita2 - densidade2;
 
 //dados
+	printf ("==============================\n");
+
+
 	printf ("CIDADE 1: \n");
 	printf ("ESTADO: %c \n", estado1);
-	printf ("CÁDIGO CARTA: %s \n", code1);
+	printf ("CÓDIGODIGO CARTA: %s \n", code1);
 	printf ("NOME: %s \n", nome1);
 	printf ("POPULAÇÃO: %d \n", population1);
 	printf ("AREA: %.2f km quadrados \n", area1);
 	printf ("PIB: %.2f \n", pib1);
-	printf ("PONTOS TURÍSTICOS: %d \n", turismo1);
+	printf ("PONTOS TURíSTICOS: %d \n", turismo1);
 	printf ("PIB PER CAPITA: %.2f \n", pibcapita1);
 	printf ("DENSIDADE POPULACIONAL: %.2f hab/km2 \n", densidade1);
+	printf ("SuperPoder: %.2f \n", superpower1);
 
 	printf ("==============================\n");
 
 	printf ("CIDADE 2: \n");
 	printf ("ESTADO: %c \n", estado2);
-	printf ("CCDIGO CARTA: %s \n", code2);
+	printf ("CóDIGO CARTA: %s \n", code2);
 	printf ("NOME: %s \n", nome2);
 	printf ("POPULAÇÃO: %d \n", population2);
 	printf ("AREA: %.2f km quadrados \n", area2);
 	printf ("PIB: %.2f bilhC5es de reais \n", pib2);
-	printf ("PONTOS TURÍSTICOS: %d \n", turismo2);
+	printf ("PONTOS TURiSTICOS: %d \n", turismo2);
 	printf ("PIB PER CAPITA: %.2f \n", pibcapita2);
-	printf ("DENSIDADE POPULACIONAL: %.2f hab/km2", densidade2);
+	printf ("DENSIDADE POPULACIONAL: %.2f hab/km2\n", densidade2);
+	printf ("SuperPoder: %.2f \n", superpower2);
 
+	printf ("==============================\n");
+	
+	if (population1 > population2) {
+	  printf ("População: Cidade 1 venceu (1)\n");
+	}
+	else {
+	  printf ("População: Cidade 2 venceu(0)\n");
+	}
+	
+	if (area1 > area2) {
+	  printf ("Área: Cidade 1 venceu (1)\n");
+	}
+	else {
+	  printf ("Área: Cidade 2 venceu(0)\n");
+	}
+	
+	if ( pib1 > pib2 ) {
+	  printf ("PIB: Cidade 1 venceu (1)\n");
+	}
+	else {
+	  printf ("PIB: Cidade 2 venceu(0)\n");
+	}
+	
+	if ( turismo1 > turismo2 ) {
+	  printf ("P. Turísticos: Cidade 1 venceu (1)\n");
+	}
+	else {
+	  printf ("P. Turísticos: Cidade 2 venceu(0)\n");
+	}
+
+	if ( densidade1 < densidade2 ) {
+	  printf ("Densidade Populacional: Cidade 1 venceu (1)\n");
+	}
+	else {
+	  printf ("Densidade Populacional: Cidade 2 venceu(0)\n");
+	}
+	
+	if ( pibcapita1 > pibcapita2) {
+	  printf ("PIB percapita: Cidade 1 venceu (1)\n");
+	}
+	else {
+	  printf ("PIB percapita: Cidade 2 venceu(0)\n");
+	}
+	
+	if ( superpower1 > superpower2 ) {
+	  printf ("Super Poder: Cidade 1 venceu (1)\n");
+	}
+	else {
+	  printf ("Super Poder: Cidade 2 venceu(0)\n");
+	}
+	
 	return 0;
 }
